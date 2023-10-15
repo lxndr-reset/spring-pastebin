15915275a1e2ad040f888a527ca128c283528baa
lxndr <lxndr.nonstop@gmail.com>
2023-10-15 16:17:20 +0300

    Updated Executor method and caffeineCacheManager bean
    Reconfigured the Executor to use ThreadPoolTaskExecutor instead of Executors for more flexible thread pool configuration, allowing thread pool tasks to be executed concurrently. Also, adjusted caffeineCacheManager's allowNullValues property from false to true. This resolves an error where a hashed empty string was being stored due to non-null value constraints.

--- src/main/java/com/pastebin/config/AppConfig.java
+++ src/main/java/com/pastebin/config/AppConfig.java
@@ -9,12 +9,10 @@ import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.ViewResolver;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -22,25 +20,32 @@ import java.util.concurrent.TimeUnit;
 @EnableCaching
 @EnableAsync
 public class AppConfig {
+
     @Bean
     public Executor executor() {
-        return Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors());
+        executor.setThreadNamePrefix("CustomAsyncExecutor-");
+        executor.initialize();
+
+        return executor;
     }
 
     @Bean
     public CacheManager cacheManager() {
-        Caffeine<Object, Object> caffeine = Caffeine.newBuilder()
-                .expireAfterWrite(30, TimeUnit.MINUTES)
+        Caffeine<Object, Object> caffeine = Caffeine.newBuilder().expireAfterWrite(30, TimeUnit.MINUTES)
                 .executor(executor());
 
         CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
         caffeineCacheManager.setCaffeine(caffeine);
+
         return caffeineCacheManager;
     }
 
     @Bean
     public CacheManagerCustomizer<CaffeineCacheManager> caffeineCacheManagerCustomizer() {
-        return caffeineCacheManager -> caffeineCacheManager.setAllowNullValues(false);
+        return caffeineCacheManager -> caffeineCacheManager.setAllowNullValues(true);
     }
 
 
