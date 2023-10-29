d0a41d4f40df5a2ce2a3170b62a16078a77bc233
lxndr <lxndr.nonstop@gmail.com>
2023-10-29 23:44:06 +0300

    Add MessageServiceTest for soft deletion functionality
    This commit introduces `MessageServiceTest` which tests MessageService's method `softDeleteByValue`. This ensures successful soft deletion when a valid message is provided and throws a clear `NoSuchElementException` for both non-existing and already soft deleted messages. Adding these tests enhance reliability and ease of debugging.

---
+++ src/test/java/com/pastebin/service/MessageServiceTest.java
@@ -0,0 +1,57 @@
+package com.pastebin.service;
+
+import com.pastebin.entity.Message;
+import com.pastebin.entity.ShortURL;
+import com.pastebin.entity.date.ValidTime;
+import com.pastebin.repository.MessageRepo;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MessageServiceTest {
+    static RestTemplate restTemplate;
+
+    @InjectMocks
+    MessageService messageService;
+
+    @Mock
+    MessageRepo messageRepo;
+
+    @BeforeAll
+    static void beforeAll() {
+        restTemplate = new RestTemplate();
+    }
+
+    @Test
+    void softDeleteByValue() {
+        Message testMessage = new Message("Test1", new ShortURL("b"), ValidTime.THREE_MONTHS);
+        String value = testMessage.getShortURL().getUrlValue();
+        Optional<Message> optionalMessage = Optional.of(testMessage);
+
+        when(messageRepo.findMessageByShortURLUrlValue(value)).thenReturn(optionalMessage);
+
+        messageService.softDeleteByValue(value).join();
+
+        verify(messageRepo, times(1)).findMessageByShortURLUrlValue(value);
+
+        Exception exception = assertThrows(NoSuchElementException.class, () ->
+                messageService.findByShortURLValue(value).join()
+        );
+
+        assertEquals("Message on link https://localhost:8080/message/get/" + value + " is deleted",
+                exception.getMessage()
+        );
+    }
+}
