5a338f2de31c9f6b0663df62d3d924531eb4b121
lxndr <lxndr.nonstop@gmail.com>
2023-11-01 21:08:24 +0300

    "Refactor application properties for environment profiles"
    This commit introduces environment-specific configuration, separating the application properties into local and docker profiles. "application-local.properties" now corresponds to the default and local development settings, with circular references in spring bean removed - not needed for our application. A new file "application-docker.properties" has been added for the Docker profile, containing database configurations specific for the Docker environment.
    
    Also, update in AppConfig.java to dynamically load the appropriate property file based on the currently active profile. This allows for a better separation of configuration and facilitates managing configurations between different environments (local and Docker in this case).

--- src/main/java/com/pastebin/config/AppConfig.java
+++ src/main/java/com/pastebin/config/AppConfig.java
@@ -3,30 +3,46 @@ package com.pastebin.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.zaxxer.hikari.HikariDataSource;
+import org.hibernate.property.access.spi.PropertyAccessBuildingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
+import java.awt.color.ProfileDataException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebMvc
 @EnableCaching
 @EnableAsync
 public class AppConfig {
+
+    private final Environment environment;
+
+    @Autowired
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public Executor executor() {
@@ -59,10 +75,20 @@ public class AppConfig {
     @Bean
     public DataSource dataSource() {
         Properties properties;
+        String propertyFileName = "application-local.properties"; //default
+
         try {
-            properties = PropertiesLoaderUtils.loadAllProperties("application.properties");
+            if (containsProperty("local")) {
+                propertyFileName = "application-local.properties";
+            } else if (containsProperty("docker")) {
+                propertyFileName = "application-docker.properties";
+            } else {
+                throw new ProfileDataException("Not contains 'local' or 'docker' profile");
+            }
+
+            properties = PropertiesLoaderUtils.loadAllProperties(propertyFileName);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to load properties from application.properties", e);
+            throw new RuntimeException("Failed to load properties from " + propertyFileName, e);
         }
 
         String dataSourcePassword = properties.getProperty("spring.datasource.password");
@@ -86,5 +112,16 @@ public class AppConfig {
         }
 
         return dataSource;
+    }
+
+    private boolean containsProperty(String ... propertyNames) {
+        HashSet<String> profiles = new HashSet<>(Arrays.asList(environment.getActiveProfiles()));
+        for (String name : propertyNames) {
+            if (profiles.contains(name)){
+                return true;
+            }
+        }
+
+        return false;
     }
 }
