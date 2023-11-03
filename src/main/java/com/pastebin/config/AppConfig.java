3c7d42391e033b262848bd2edd2dffe7edebf81e
lxndr <lxndr.nonstop@gmail.com>
2023-11-04 01:27:54 +0300

    "Refactor AppConfig to use @Value for datasource properties"
    In this changeset, AppConfig was refactored to utilize Spring's @Value annotation for reading the datasource properties directly from the application's active profile properties file. This approach simplifies the data source configuration, making it more readable, maintainable, and in line with spring conventions. The manual property loading code was removed as it was no longer necessary.

--- src/main/java/com/pastebin/config/AppConfig.java
+++ src/main/java/com/pastebin/config/AppConfig.java
@@ -4,25 +4,20 @@ package com.pastebin.config;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
-import java.awt.color.ProfileDataException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Properties;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
@@ -30,14 +25,23 @@ import java.util.concurrent.TimeUnit;
 @EnableWebMvc
 @EnableCaching
 @EnableAsync
+@PropertySource("classpath:application-${spring.profiles.active}.properties")
 public class AppConfig {
 
-    private final Environment environment;
     private final Executor executor;
 
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dataSourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dataSourcePassword;
+
+
     @Autowired
-    public AppConfig(Environment environment, Executor executor) {
-        this.environment = environment;
+    public AppConfig(Executor executor) {
         this.executor = executor;
     }
 
@@ -59,34 +63,8 @@ public class AppConfig {
         return caffeineCacheManager -> caffeineCacheManager.setAllowNullValues(true);
     }
 
-
     @Bean
     public DataSource dataSource() {
-        Properties properties;
-        String propertyFileName = "application-local.properties"; //default
-
-        try {
-            if (containsProperty("local")) {
-                propertyFileName = "application-local.properties";
-            } else if (containsProperty("docker")) {
-                propertyFileName = "application-docker.properties";
-            } else {
-                throw new ProfileDataException("Not contains 'local' or 'docker' profile");
-            }
-
-            properties = PropertiesLoaderUtils.loadAllProperties(propertyFileName);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load properties from " + propertyFileName, e);
-        }
-
-        String dataSourcePassword = properties.getProperty("spring.datasource.password");
-        String dataSourceUsername = properties.getProperty("spring.datasource.username");
-        String dataSourceUrl = properties.getProperty("spring.datasource.url");
-
-        if (dataSourcePassword == null || dataSourceUsername == null || dataSourceUrl == null) {
-            throw new IllegalArgumentException("DataSource configuration is missing");
-        }
-
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setJdbcUrl(dataSourceUrl);
         dataSource.setPassword(dataSourcePassword);
@@ -94,22 +72,10 @@ public class AppConfig {
 
         //to check if connection available
         try (Connection connection = dataSource.getConnection()) {
-
         } catch (SQLException e) {
             throw new RuntimeException("Failed to establish DB connection", e);
         }
 
         return dataSource;
-    }
-
-    private boolean containsProperty(String... propertyNames) {
-        HashSet<String> profiles = new HashSet<>(Arrays.asList(environment.getActiveProfiles()));
-        for (String name : propertyNames) {
-            if (profiles.contains(name)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
