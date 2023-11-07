a1930bd29750cc501b0fed49c98411f1d33ee299
lxndr <lxndr.nonstop@gmail.com>
2023-11-07 04:07:17 +0300

    Refactor UserDetailsService to adhere to Spring Security
    Removed AuthenticationProvider and related classes from the UserDetailsService. Instead, we are using SpringSecurity's UserDetailsService directly. Removed an unnecessary call to findUserByEmail since we are already assuming the user is checked and is present in the database. This is to reduce redundancy, adhere to best practices and minimize the potential for security flaws.

--- src/main/java/com/pastebin/service/user_details/UserDetailsService.java
+++ src/main/java/com/pastebin/service/user_details/UserDetailsService.java
@@ -3,14 +3,12 @@ package com.pastebin.service.user_details;
 import com.pastebin.entity.User;
 import com.pastebin.service.entityService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
+
 @Service
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
     private final UserService userService;
@@ -27,9 +25,14 @@ public class UserDetailsService implements org.springframework.security.core.use
         return new com.pastebin.service.user_details.UserDetails(user);
     }
 
+    /**
+     * Assuming that user is already checked and database contains it
+     *
+     * @param user
+     * @return
+     * @throws UsernameNotFoundException
+     */
     public UserDetails loadByUser(User user) throws UsernameNotFoundException {
-        userService.findUserByEmail(user.getEmail()).orElseThrow(() -> new NoSuchElementException("User not found"));
-
         return new com.pastebin.service.user_details.UserDetails(user);
     }
 }
